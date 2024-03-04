f434bfe244999f98e68c409753ac13b46b6df36c
Tracing <52660906+SijieLuo@users.noreply.github.com>
2024-03-04 14:30:31 +0800

    Update val.py

--- val.py
+++ val.py
@@ -342,11 +342,11 @@ def run(
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default=ROOT / 'data/data.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default=r'I:\yolov5-master\yolov5-master\runs\train\exp95-PR_6933_LA_BEIYONG-lcd_news\weights\best.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default=r'', help='model path(s)')
     parser.add_argument('--batch-size', type=int, default=16, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=300, help='maximum detections per image')
     parser.add_argument('--task', default='val', help='train, val, test, speed or study')
     parser.add_argument('--device', default=' ', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
